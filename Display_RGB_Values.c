6bd0ddd79dea6888d81ae37c0e60c39ad7a020f0
Valerian Coelho <valerian27012004@gmail.com>
2023-06-05 22:21:42 +0530

    Image Displaying Logic Implemented

--- Display_RGB_Values.c
+++ Display_RGB_Values.c
@@ -1,26 +1,29 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <stdlib.h>
 
 int main() {
     int width, height, channels;
     unsigned char* image_data = stbi_load("image.jpg", &width, &height, &channels, STBI_rgb);
+    system("cls");
 
     if (image_data == NULL) {
         printf("Unable to load the image\n");
         return -1;
     }
 
-    // Iterate over each pixel of the image
-    for (int i = 0; i < width * height; ++i) {
-        int pixel_index = i * channels;
-        int red = image_data[pixel_index];
-        int green = image_data[pixel_index + 1];
-        int blue = image_data[pixel_index + 2];
-        
-        // Print the RGB values
-        printf("Pixel %d: R=%d, G=%d, B=%d\n", i, red, green, blue);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int index = i * width + j;
+            int pixel_index = index * channels;
+            int red = image_data[pixel_index];
+            int green = image_data[pixel_index + 1];
+            int blue = image_data[pixel_index + 2];
+            printf("\033[48;2;%d;%d;%dm  \033[0m", red, green, blue);
+        }
+        printf("\n");
     }
-
     stbi_image_free(image_data);
+
     return 0;
 }
