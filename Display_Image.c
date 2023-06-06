a5b9cf5bb19bdfbb3617bcae3de1b1f168aa4961
Valerian Coelho <valerian27012004@gmail.com>
2023-06-06 18:41:30 +0530

    logic added for display the image

--- Display_Image.c
+++ Display_Image.c
@@ -5,6 +5,9 @@
 #include <dirent.h>
 #include <string.h>
 #include "Options.h"
+#include <conio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define MAX_FILES 100
 #define MAX_FILENAME_LENGTH 40
@@ -41,18 +44,38 @@ int main() {
             printf("Removing the last folder\n");
         }
         else if (strchr(options[choice], '.') != NULL) {
-            printf("It's a file extension\n");
+            char directory[MAX_FILENAME_LENGTH] = "./";
+            strcat(directory, options[choice]);
+
+            int width, height, channels;
+            const char* filePath = directory;
+            unsigned char* image_data = stbi_load(filePath, &width, &height, &channels, STBI_rgb);
+            system("cls");
+
+            if (image_data == NULL) {
+                printf("Unable to load the image\n");
+            }
+            else{
+                for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    int index = i * width + j;
+                    int pixel_index = index * channels;
+                    int red = image_data[pixel_index];
+                    int green = image_data[pixel_index + 1];
+                    int blue = image_data[pixel_index + 2];
+                    printf("\033[48;2;%d;%d;%dm  \033[0m", red, green, blue);
+                }
+                printf("\n");
+                }
+                stbi_image_free(image_data);
+            }
+            getch();
+            goto label;
         }
         else {
-            char* directory = (char*)malloc(PATH_MAX * sizeof(char));
-            if (directory == NULL) {
-                perror("Memory allocation failed");
-                return 1;
-            }
-            strcpy(directory, "./");
+            char directory[MAX_FILENAME_LENGTH] = "./";
             strcat(directory, options[choice]);
             chdir(directory);
-            free(directory);
             goto label;
             printf("It's a folder\n");
         }
@@ -67,28 +90,7 @@ int main() {
 // #include <stdlib.h>
 
 // int main() {
-//     int width, height, channels;
-//     const char* filePath = "D:/Valerian/Coding/C/Personal Projects/Terminal Image Viewer/image.jpg";
-//     unsigned char* image_data = stbi_load(filePath, &width, &height, &channels, STBI_rgb);
-//     system("cls");
-
-//     if (image_data == NULL) {
-//         printf("Unable to load the image\n");
-//         return -1;
-//     }
-
-//     for (int i = 0; i < height; i++) {
-//         for (int j = 0; j < width; j++) {
-//             int index = i * width + j;
-//             int pixel_index = index * channels;
-//             int red = image_data[pixel_index];
-//             int green = image_data[pixel_index + 1];
-//             int blue = image_data[pixel_index + 2];
-//             printf("\033[48;2;%d;%d;%dm  \033[0m", red, green, blue);
-//         }
-//         printf("\n");
-//     }
-//     stbi_image_free(image_data);
+    
 
 //     return 0;
 // }
